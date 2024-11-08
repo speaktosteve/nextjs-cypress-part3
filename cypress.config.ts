f52e604eee7f54219c4ef4df13c0eafc5370517f
Steve Smith <steve.a.smith@avanade.com>
2024-11-08 15:32:22 +0000

    move e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
+    component: {
+        devServer: {
+            framework: 'next',
+            bundler: 'webpack',
+        },
     },
-  },
 
-  watchForFileChanges: true,
+    watchForFileChanges: true,
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        specPattern: 'src/app/components/**/*.e2e.cy.{js,ts,jsx,tsx}', // Change this to your preferred folder
     },
-  },
-});
+})
